147a7099b599fd9928141aed5da5acda88ecc65d
Rushan Kashapov <kashapovrush@mail.ru>
2023-12-23 18:42:46 +0300

    implementation apiService finished (23.12.23)

--- app/src/main/java/com/kashapovrush/cofeeshopstest/domain/AuthRepository.kt
+++ app/src/main/java/com/kashapovrush/cofeeshopstest/domain/AuthRepository.kt
@@ -2,11 +2,7 @@ package com.kashapovrush.cofeeshopstest.domain
 
 import com.kashapovrush.cofeeshopstest.data.model.Token
 import com.kashapovrush.cofeeshopstest.data.model.User
-import com.kashapovrush.cofeeshopstest.presentation.signInScreen.AuthState
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 import retrofit2.Call
-import retrofit2.Response
 
 interface AuthRepository {
 
--- app/src/main/java/com/kashapovrush/cofeeshopstest/domain/LoginUserUseCase.kt
+++ app/src/main/java/com/kashapovrush/cofeeshopstest/domain/LoginUserUseCase.kt
@@ -2,9 +2,6 @@ package com.kashapovrush.cofeeshopstest.domain
 
 import com.kashapovrush.cofeeshopstest.data.model.Token
 import com.kashapovrush.cofeeshopstest.data.model.User
-import com.kashapovrush.cofeeshopstest.presentation.signInScreen.AuthState
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 import retrofit2.Call
 import javax.inject.Inject
 
